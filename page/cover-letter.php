d7a0a33881dd8138ff2621724e8d02e063557041
thaihung019 <thaihung019@gmail.com>
2017-02-20 16:13:20 +0700

    cv

---
+++ page/cover-letter.php
@@ -0,0 +1,58 @@
+<section class="col s12 m12 l8 section" id="section">
+                    <div class="cover-latter z-depth-1">
+                        <div class="cover-title">
+                            <span>20 February 2017</span>
+                            <h2>On Thai Hung</h2>
+                            <p>
+                                140/6 Le Duc Tho Street - Ward 6<br>
+                                Go Vap District <br>
+                                Ho Chi Minh City
+                            </p>
+                        </div>
+                        <h4 class="cover-title">Dear Sir/Madam</h4>
+                        <p>I am writing to you to apply for the PHP programmer position that I was recommended from your colleague.</p>
+
+                        <p>As you can see from my CV page, I am currently a Sophomore at Aptech International Information Technology Education System, majoring in Information Technology. I am also a Junior at Tran Dai Nghia University with the same major. Therefore, my English is quite good to read book and interact with native speakers, my foreign friends and as well.</p>
+                        
+                        <p>In addition, I took part in lots of English activities in another University. The result is that I intended and won Second place in Young Debate Championship celebrated by Vietnamese-German University 2015. Moreover, I joined in Aptech Apkathon to making some thing "cool" and achieved the UI/UX design. Having experiences when work in team and in activitive environment. I know how to make handle my part of work and control self-pressure.</p>
+                        
+                        <p>Working for your company is my important target. I found this is a good chance for me to improve my working skills, my English as well as gaining experience from such a dynamic and professional working environment. A little thing is that I have an opportunity to be financially independent from my family. I would try my best to dedicate my knowledge, my enthusiasm when working in your company.</p>
+
+                        <p>Thank you for taking the time to consider my application and I am looking forward to hearing from you in the near future.</p>
+                        <div class="cover-title">
+                            <p>Your Sincerely</p>
+                        </div>
+                        <div class="signature">
+                            <!-- <img src="assets/images/sig.png" alt="" class="img-responsive"></img>                            -->
+                            <h4>On Thai Hung</h4>
+                            <h5> Programmer </h5>                          
+                        </div>
+                    </div>
+                <!-- =========================================
+                   Refference 
+                ==========================================-->
+                    <!-- <div class="section-wrapper refarances z-depth-1">
+                        <div class="row"> 
+                            <div class="section-icon col s12 m12 l2">
+                                <i class="fa fa-graduation-cap"></i>
+                            </div>
+                            <div class="custom-content col s12 m12 l10 wow fadeIn a1" data-wow-delay="0.1s">
+                                <h2>Refarances</h2>
+
+                                <div class="custom-content-wrapper wow fadeIn a2" data-wow-delay="0.2s" >
+                                    <h3>Hasin Hayder</h3>
+                                    <span>Head of Ideas, Themebucket </span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+
+                                <div class="custom-content-wrapper wow fadeIn a3" data-wow-delay="0.3s" >
+                                    <h3>Mizanur Rahman</h3>
+                                    <span>Founder & CEO, Template Ocean</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                </section>
+            </div><!-- end row -->
+        </div> <!-- end container -->
---
+++ page/home.php
@@ -0,0 +1,122 @@
+ <!-- =========================================
+                   Work experiences
+                ==========================================-->
+
+                <section class="col s12 m12 l8 section">
+                    <div class="row">
+                        <!-- <div class="section-wrapper z-depth-1">                            
+                            <div class="section-icon col s12 m12 l2">
+                                <i class="fa fa-suitcase"></i>
+                            </div>
+                            <div class="custom-content col s12 m12 l10 wow fadeIn a1" data-wow-delay="0.1s">
+                                <h2>Work Experience</h2>
+
+                                <div class="custom-content-wrapper wow fadeIn a2" data-wow-delay="0.2s">
+                                    <h3>UI/UX Designer <span>@Academy</span></h3>
+                                    <span>JAN 2013 - DEC 2013 </span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                        labore et dolore magna aliqua. </p>
+                                </div>
+                                <div class="custom-content-wrapper wow fadeIn a3" data-wow-delay="0.3s">
+                                    <h3>Creative Director <span>@DeviserWeb</span></h3>
+                                    <span>JAN 2013 - DEC 2013 </span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                                        dolore magna aliqua. </p>
+                                </div>
+                                <div class="custom-content-wrapper wow fadeIn a4" data-wow-delay="0.4s">
+                                    <h3>Graphics Designer <span>@Creative Wrold</span></h3>
+                                    <span>JAN 2013 - DEC 2013 </span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                        labore et dolore magna aliqua. </p>
+                                </div>
+                            </div>                            
+                        </div> -->
+
+                        <!-- ========================================
+                         Education 
+                        ==========================================-->
+
+                        <div class="section-wrapper z-depth-1">
+                            <div class="section-icon col s12 m12 l2">
+                                <i class="fa fa-graduation-cap"></i>
+                            </div>
+                            <div class="custom-content col s12 m12 l10 wow fadeIn a1" data-wow-delay="0.1s" >
+                                <h2>Education </h2>
+                                
+                                <div class="custom-content-wrapper wow fadeIn a2" data-wow-delay="0.2s" >
+                                    <h3>Sophomore <span>@Aptech Computer Education</span></h3>
+                                    <span>From JUNE 2016 </span>
+                                    <p>Getting 90% point in the first year project - PHP project.<br>
+                                    Knowing how to use PHP code to interrogate data from mySQL.<br>
+                                    Get high knowledge in HTML and CSS.</p>
+                                </div>
+                                <div class="custom-content-wrapper wow fadeIn a3" data-wow-delay="0.3s" >
+                                    <h3>Junior <span>@Tran Dai Nghia University</span></h3>
+                                    <span>From SEP 2014 </span>
+                                    <p>Learned how to manage a project.<br>
+                                    Doing teamwork well. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- ========================================
+                              Achievement
+                        ==========================================-->
+
+                        <div class="section-wrapper z-depth-1">                            
+                            <div class="section-icon col s12 m12 l2">
+                                <i class="fa fa-trophy"></i>
+                            </div>
+                            <div class="custom-content col s12 m12 l10 wow fadeIn a1" data-wow-delay="0.1s">
+                                <h2>Achievement</h2>
+
+                                <div class="custom-content-wrapper wow fadeIn a2" data-wow-delay="0.2s">
+                                    <h3>Second Place at YDB Championship <span>@VietNam-Germany University</span></h3>
+                                    <span>NOV 2015 </span>
+                                    <p>In that champion I learned much about how to work with colleague, how to control emotion when under pressure. As a result, I defeated many competitor teams to win the Second Place. </p>
+                                </div>
+                                <div class="custom-content-wrapper wow fadeIn a3" data-wow-delay="0.3s">
+                                    <h3>UI/UX Design Apkathon <span>@Aptech Computer Education</span></h3>
+                                    <span>JULY 2016 </span>
+                                    <p>Me and my teamates spent 36 hours to constanly coding to make our first shopping website.</p>
+                                </div>
+                            </div>                            
+                        </div>
+
+
+                        <!-- ========================================
+                              Intertests 
+                        ==========================================-->
+
+                        <div class="section-wrapper z-depth-1">                           
+                            <div class="section-icon col s12 m12 l2">
+                                <i class="fa fa-plane"></i>
+                            </div>
+                            <div class="interests col s12 m12 l10 wow fadeIn" data-wow-delay="0.1s"> 
+                                <h2>Interestes </h2>
+                                <ul> <!-- interetsr icon start -->
+                                    <li><i class="fa fa-camera-retro tooltipped" data-position="top" data-delay="50" data-tooltip="Photography"></i></li>
+                                    <li><i class="fa fa-gamepad tooltipped" data-position="top" data-delay="50" data-tooltip="Playing Game"></i></li>
+                                    <li><i class="fa fa-headphones tooltipped" data-position="top" data-delay="50" data-tooltip="Music"></i></li>
+                                    <li><i class="fa fa-group tooltipped" data-position="top" data-delay="50" data-tooltip="Chatting"></i></li>
+                                    <li><i class="fa fa-lightbulb-o tooltipped" data-position="top" data-delay="50" data-tooltip="Searching"></i></li>
+                                </ul> <!-- interetsr icon end -->
+                            </div>                          
+                        </div>
+                        <!-- =======================================
+                          portfolio Website
+                        ==========================================-->
+
+                        <div class="section-wrapper z-depth-1 wow fadeIn" data-wow-delay="0.1s">                            
+                            <div class="col s12 m12 l10 website right" >
+                                <div class="row">
+                                    <div class="col s12 m12 l6">
+                                       <span><a href="#">www.onthaihung.github.io</a></span>
+                                    </div>
+                                 
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end row -->
+                </section><!-- end section -->
+            </div> <!-- end row -->
+        </div>  <!-- end container -->
